28d7308e03fdf77b0e110bbf1c5d5c499504342b
Jim Irrer <irrer@umich.edu>
2021-03-03 06:23:31 -0500

    Marked meaningful changes with // jjjjj

--- src/main/java/org/aqaclient/ConfirmDicomComplete.scala
+++ src/main/java/org/aqaclient/ConfirmDicomComplete.scala
@@ -1,6 +1,9 @@
 package org.aqaclient
 
-import edu.umro.ScalaUtil.{FileUtil, Logging, PrettyXML}
+import edu.umro.ScalaUtil.FileUtil
+import edu.umro.ScalaUtil.Logging
+import edu.umro.ScalaUtil.PrettyXML
+import edu.umro.ScalaUtil.Trace
 import org.aqaclient.Upload.UploadSet
 
 import java.io.File
@@ -8,14 +11,15 @@ import java.util.Date
 import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.xml.{Elem, XML}
+import scala.xml.Elem
+import scala.xml.XML
 
 /**
- * After a data set is uploaded, keep track of the DICOM series involved in case they
- * change. It is quite possible that more images were produced after the initial DICOM
- * retrieval, in which case the data set should be re-uploaded with the more complete
- * set of images.
- */
+  * After a data set is uploaded, keep track of the DICOM series involved in case they
+  * change. It is quite possible that more images were produced after the initial DICOM
+  * retrieval, in which case the data set should be re-uploaded with the more complete
+  * set of images.
+  */
 object ConfirmDicomComplete extends Logging {
 
   private case class ConfirmState(uploadSet: UploadSet, InitialUploadTime: Date = new Date, size: Option[Int] = None) {
@@ -25,81 +29,100 @@ object ConfirmDicomComplete extends Logging {
       else ClientUtil.listFiles(uploadSet.imageSeries.dir).size
     }
 
+    Trace.trace()
     // jjjjj
     def toXml: Elem = {
+      Trace.trace()
       <ConfirmDicomComplete>
         <InitialUploadTime>
           {Series.xmlDateFormat.format(new Date)}
         </InitialUploadTime>{uploadSet.procedure.node}<ImageSeries size={imageSeriesSize.toString}>
         {uploadSet.imageSeries.SeriesInstanceUID}
-      </ImageSeries>{if (uploadSet.reg.isDefined) <Reg>
+      </ImageSeries>{
+        if (uploadSet.reg.isDefined) <Reg>
         {uploadSet.reg.get.SeriesInstanceUID}
-      </Reg>}{if (uploadSet.plan.isDefined) <Plan>
+      </Reg>
+      }{
+        if (uploadSet.plan.isDefined) <Plan>
         {uploadSet.reg.get.SeriesInstanceUID}
-      </Plan>}
+      </Plan>
+      }
       </ConfirmDicomComplete>
     }
 
     def fileName: String = {
+      Trace.trace()
       val text = uploadSet.imageSeries.PatientID + "_" +
         uploadSet.imageSeries.Modality + "_" +
         Series.xmlDateFormat.format(uploadSet.imageSeries.dataDate) + "_" +
         uploadSet.procedure + "_" +
         uploadSet.imageSeries.SeriesInstanceUID + ".xml"
 
+      Trace.trace()
       FileUtil.replaceInvalidFileNameCharacters(text.replace(' ', '_'), '_').replaceAll("___*", "_")
     }
 
     def file = new File(ClientConfig.confirmDicomCompleteDir, fileName)
 
     def persist(): Unit = {
+      Trace.trace()
       val text = PrettyXML.xmlToText(toXml)
       val file = new File(ClientConfig.confirmDicomCompleteDir, fileName)
       FileUtil.writeFile(file, text)
+      Trace.trace()
     }
 
     val timeout = new Date(ClientConfig.ConfirmDicomCompleteTimeout_ms + InitialUploadTime.getTime)
+    Trace.trace()
 
     def msRemaining: Long = timeout.getTime - System.currentTimeMillis
+    Trace.trace()
 
     def isActive: Boolean = msRemaining > 0
 
     def terminate(): Unit = {
+      Trace.trace()
       file.delete
       logger.info("Completed confirmation of DICOM upload and have deleted " + file.getAbsolutePath)
     }
   }
 
   /**
-   * Redo the given upload.
-   */
+    * Redo the given upload.
+    */
   private def redoUpload(confirmState: ConfirmState): Option[UploadSet] = {
+    Trace.trace()
     Series.update(confirmState.uploadSet.imageSeries.SeriesInstanceUID) match {
       case Some(series) =>
         val newUploadSet = confirmState.uploadSet.copy(imageSeries = series)
         Upload.upload(newUploadSet)
+        Trace.trace()
         Some(newUploadSet)
       case _ =>
         logger.warn("Unable to update series " + confirmState.uploadSet.imageSeries)
+        Trace.trace()
         None
     }
   }
 
   /**
-   * Monitor post-upload by checking to see if any additional image slices are available from the
-   * source PACS. If they are, then get them and redo the upload. If no more slices appear within
-   * a configured timeout (<code>ClientConfig.ConfirmDicomCompleteInterval_sec</code>) then stop
-   * monitoring it.
-   *
-   * When this function is called it will make at least one attempt to redo the upload. This is to
-   * cover the case where the service was restarted, and even though a lot of time has elapsed, the
-   * upload was not monitored during that period and so should be checked.
-   */
+    * Monitor post-upload by checking to see if any additional image slices are available from the
+    * source PACS. If they are, then get them and redo the upload. If no more slices appear within
+    * a configured timeout (<code>ClientConfig.ConfirmDicomCompleteInterval_sec</code>) then stop
+    * monitoring it.
+    *
+    * When this function is called it will make at least one attempt to redo the upload. This is to
+    * cover the case where the service was restarted, and even though a lot of time has elapsed, the
+    * upload was not monitored during that period and so should be checked.
+    */
   @tailrec
   private def monitor(confirmState: ConfirmState): Unit = {
+    Trace.trace()
     def msRemaining = confirmState.timeout.getTime - System.currentTimeMillis
 
-    logger.info("Before sleep.  Monitoring DICOM upload with timeout at: " + confirmState.timeout + "    ms remaining: " + msRemaining + "/" + confirmState.msRemaining + " for " + confirmState.file.getAbsolutePath)
+    logger.info(
+      "Before sleep.  Monitoring DICOM upload with timeout at: " + confirmState.timeout + "    ms remaining: " + msRemaining + "/" + confirmState.msRemaining + " for " + confirmState.file.getAbsolutePath
+    )
 
     Thread.sleep(ClientConfig.ConfirmDicomCompleteInterval_ms)
     logger.info("After sleep. Monitoring DICOM upload with timeout at: " + confirmState.timeout + "    ms remaining: " + confirmState.msRemaining + " for " + confirmState.file.getAbsolutePath)
@@ -107,6 +130,7 @@ object ConfirmDicomComplete extends Logging {
 
     // if the number of slices changed, then redo upload.
     if (newSize != confirmState.imageSeriesSize) {
+      Trace.trace()
       logger.info("Need to redo upload.  Size was: " + confirmState.imageSeriesSize + " but changed to " + newSize + "  ms remaining: " + confirmState.msRemaining + "  for " + confirmState.fileName)
 
       redoUpload(confirmState) match {
@@ -122,6 +146,7 @@ object ConfirmDicomComplete extends Logging {
           }
       }
     } else {
+      Trace.trace()
       if (confirmState.isActive) {
         monitor(confirmState)
       } else {
@@ -131,9 +156,10 @@ object ConfirmDicomComplete extends Logging {
   }
 
   /**
-   * An initial upload of the given data set has been done. Put it on the list to be monitored for updates.
-   */
+    * An initial upload of the given data set has been done. Put it on the list to be monitored for updates.
+    */
   def confirmDicomComplete(uploadSet: UploadSet): Unit = {
+    Trace.trace()
     val confirmState = ConfirmState(uploadSet)
     confirmState.persist()
     Future {
@@ -142,16 +168,17 @@ object ConfirmDicomComplete extends Logging {
   }
 
   /**
-   * Read a persisted <code>ConfirmState</code> from a file. If there are any errors then log them and
-   * return None.
-   */
+    * Read a persisted <code>ConfirmState</code> from a file. If there are any errors then log them and
+    * return None.
+    */
   private def fromFile(xmlFile: File): Option[ConfirmState] = {
     try {
+      Trace.trace()
       val xml = XML.loadFile(xmlFile)
 
       /**
-       * Attempt to get a series that may or may not be specified.
-       */
+        * Attempt to get a series that may or may not be specified.
+        */
       def getOptSeries(tag: String): Option[Series] = {
         try {
           Series.get((xml \ tag).head.text.trim)
@@ -168,11 +195,21 @@ object ConfirmDicomComplete extends Logging {
       val imageSeriesSize = (xml \ "ImageSeries" \ "@size").head.text.trim.toInt
       val imageSeries = Series.get(imageSeriesUID).get
 
+      Trace.trace()
+      Trace.trace("InitialUploadTime: " + InitialUploadTime)
+      Trace.trace("procedureXml: " + procedureXml)
+      Trace.trace("Procedure: " + Procedure)
+      Trace.trace("imageSeriesUID: " + imageSeriesUID)
+      Trace.trace("imageSeriesSize: " + imageSeriesSize)
+      Trace.trace("imageSeries: " + imageSeries)
+      Trace.trace()
+
       val reg = getOptSeries("Reg")
       val plan = getOptSeries("Plan")
 
       val uploadSet = UploadSet(Procedure, imageSeries, reg, plan)
       val cs = ConfirmState(uploadSet, InitialUploadTime, Some(imageSeriesSize))
+      Trace.trace()
       Some(cs)
     } catch {
       case t: Throwable =>
@@ -182,12 +219,15 @@ object ConfirmDicomComplete extends Logging {
   }
 
   /**
-   * Initialize by reading any persisted in-progress <code>ConfirmState</code> and finishing them.
-   */
+    * Initialize by reading any persisted in-progress <code>ConfirmState</code> and finishing them.
+    */
   def init(): Unit = {
+    Trace.trace()
     val confirmList = ClientUtil.listFiles(ClientConfig.confirmDicomCompleteDir).map(fromFile)
     logger.info("Number of ConfirmDicomComplete files found in " + ClientConfig.confirmDicomCompleteDir.getAbsolutePath + " : " + confirmList.size)
+    Trace.trace()
     confirmList.flatten.map(c => Future { monitor(c) })
+    Trace.trace()
   }
 
 }
--- src/main/java/org/aqaclient/EventReceiver.scala
+++ src/main/java/org/aqaclient/EventReceiver.scala
@@ -2,65 +2,79 @@ package org.aqaclient
 
 import edu.umro.EventNetClient.EventNetClient
 import edu.umro.EventNetClient.EventNetClientConfig
-import edu.umro.ScalaUtil.Trace
-import scala.xml.Elem
-import edu.umro.ScalaUtil.PrettyXML
-import scala.xml.XML
 import edu.umro.ScalaUtil.Logging
+import edu.umro.ScalaUtil.PrettyXML
+import edu.umro.ScalaUtil.Trace
+
+import scala.xml.Elem
+import scala.xml.XML
 
 object EventReceiver extends Logging {
   Trace.trace("starting")
 
   private lazy val eventNetClient: Option[EventNetClient] = {
     try {
+      Trace.trace()
       val eventNetConfig = new EventNetClientConfig(ClientConfig.AMQPBrokerHost, ClientConfig.AMQPBrokerPort, "gbtopic", "admintopic", "Aria.Event.")
+      Trace.trace()
 
       val ec = new EventNetClient(eventNetConfig, "AQAClient", 10, 10 * 1000)
       logger.info("EventNet is operational and using broker " + ClientConfig.AMQPBrokerHost + ":" + ClientConfig.AMQPBrokerPort)
+      Trace.trace()
       Some(ec)
-
     } catch {
       case t: Throwable =>
         logger.warn("EventNet is not being enabled.  Unable to connect to broker at " + ClientConfig.AMQPBrokerHost + ":" + ClientConfig.AMQPBrokerPort + " : " + fmtEx(t))
+        Trace.trace()
         None
     }
   }
 
-  val queueNameList = Seq(
-    "Aria.Event.EventPlanApproval",
-    "Aria.Event.EventPlanStatus")
+  val queueNameList = Seq("Aria.Event.EventPlanApproval", "Aria.Event.EventPlanStatus")
 
   def updatePatient(data: Array[Byte]): Unit = {
+    Trace.trace()
     val document = XML.loadString(new String(data))
+    Trace.trace()
     val PatientId = (document \ "PatientId").head.text
+    Trace.trace()
     logger.info("Received event " + document.label + " for PatientId: " + PatientId)
     if (PatientIDList.getPatientIDList.exists(p => p.trim.equalsIgnoreCase(PatientId.trim))) {
+      Trace.trace()
       logger.info("Retrieving updated list of DICOM series for PatientId: " + PatientId)
       DicomProcessing.updatePatient(PatientId)
     } else
       logger.info("PatientId " + PatientId + " is not listed in the " + PatientIDList.PatientIDFile.getAbsolutePath + " file. Ignoring event.")
+    Trace.trace()
   }
 
   /** Send an event. */
   def sendEvent(document: Elem): Unit = {
     Trace.trace
     val routingKey = "Aria.Event." + document.label
+    Trace.trace()
     val data = PrettyXML.xmlToText(document).getBytes
+    Trace.trace()
     eventNetClient match {
       case Some(ec) => ec.sendEvent("gbtopic", routingKey, data)
-      case _ => ;
+      case _        => ;
     }
+    Trace.trace()
   }
 
   /**
-   * Initialize by connecting to the AMQP broker and starting listeners.
-   */
+    * Initialize by connecting to the AMQP broker and starting listeners.
+    */
   def init(): Unit = {
     if (eventNetClient.isDefined) {
+      Trace.trace()
       logger.info("Starting EventNet interface.")
+      Trace.trace()
       queueNameList.foreach(queueName => eventNetClient.get.consumeNonDurable("gbtopic", queueName, updatePatient))
+      Trace.trace()
     } else
       logger.info("EventNet is disabled")
+    Trace.trace()
   }
 
 }
