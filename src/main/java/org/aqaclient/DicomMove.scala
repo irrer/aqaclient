5c632b552ea9159e6838bfeb2fd4a7600ebc2773
Jim Irrer <irrer@umich.edu>
2021-04-19 17:15:59 -0400

    Reformatted code.

--- src/main/java/org/aqaclient/DicomMove.scala
+++ src/main/java/org/aqaclient/DicomMove.scala
@@ -14,8 +14,8 @@ import java.io.File
 import scala.annotation.tailrec
 
 /**
- * Utility for getting DICOM via C-MOVE and caching them in the local disk.
- */
+  * Utility for getting DICOM via C-MOVE and caching them in the local disk.
+  */
 object DicomMove extends Logging {
 
   val activeDirName = "active"
@@ -37,9 +37,9 @@ object DicomMove extends Logging {
   }
 
   /**
-   * Remove files from the active directory.  There should not be any there, but it is
-   * possible if the system was shut down while a transfer was taking place.
-   */
+    * Remove files from the active directory.  There should not be any there, but it is
+    * possible if the system was shut down while a transfer was taking place.
+    */
   private def clearActiveDir(): Unit = {
     activeDir.mkdirs
     val timeout = System.currentTimeMillis + (10 * 1000)
@@ -50,28 +50,28 @@ object DicomMove extends Logging {
     }
   }
 
-
   private def moveActiveDirToSeriesDir: Option[Series] = {
     val activeList = ClientUtil.listFiles(activeDir)
     if (activeList.isEmpty) None
-    else try {
-      val alList = activeList.map(f => ClientUtil.readDicomFile(f)).filter(al => al.isRight).map(al => al.right.get)
-      val seriesDir = Series.dirOf(alList)
-      Utility.deleteFileTree(seriesDir)
-      seriesDir.getParentFile.mkdirs
-      activeDir.renameTo(seriesDir)
-      val series = Series.makeSeriesFromDicomFileDir(seriesDir)
-      Some(series)
-    } catch {
-      case t: Throwable =>
-        logger.warn("Unexpected error while moving files in active directory: " + fmtEx(t))
-        None
-    }
+    else
+      try {
+        val alList = activeList.map(f => ClientUtil.readDicomFile(f)).filter(al => al.isRight).map(al => al.right.get)
+        val seriesDir = Series.dirOf(alList)
+        Utility.deleteFileTree(seriesDir)
+        seriesDir.getParentFile.mkdirs
+        activeDir.renameTo(seriesDir)
+        val series = Series.makeSeriesFromDicomFileDir(seriesDir)
+        Some(series)
+      } catch {
+        case t: Throwable =>
+          logger.warn("Unexpected error while moving files in active directory: " + fmtEx(t))
+          None
+      }
   }
 
   /**
-   * Get a list of the SOPInstanceUIDs of the series via C-FIND
-   */
+    * Get a list of the SOPInstanceUIDs of the series via C-FIND
+    */
   private def getSliceList(SeriesInstanceUID: String): Seq[String] = {
     try {
       val al = new AttributeList
@@ -84,9 +84,11 @@ object DicomMove extends Logging {
       val alList = DicomCFind.cfind(
         ClientConfig.DICOMClient.aeTitle,
         ClientConfig.DICOMSource,
-        al, DicomCFind.QueryRetrieveLevel.IMAGE,
+        al,
+        DicomCFind.QueryRetrieveLevel.IMAGE,
         None,
-        DicomCFind.QueryRetrieveInformationModel.StudyRoot)
+        DicomCFind.QueryRetrieveInformationModel.StudyRoot
+      )
 
       def gg(al: AttributeList) = {
         val s = al.get(TagFromName.SOPInstanceUID).getSingleStringValueOrEmptyString
@@ -104,14 +106,14 @@ object DicomMove extends Logging {
   }
 
   /**
-   * Get the SOPInstanceUID of a file.
-   */
+    * Get the SOPInstanceUID of a file.
+    */
   private def fileToSopInstanceUID(file: File): Option[String] = {
     try {
       val al = new AttributeList
       al.read(file)
       al.get(TagFromName.SOPInstanceUID).getSingleStringValueOrEmptyString match {
-        case "" => None
+        case ""  => None
         case uid => Some(uid)
       }
 
@@ -123,16 +125,16 @@ object DicomMove extends Logging {
   }
 
   /**
-   * Get the list of all SOPInstanceUID in the active directory.
-   */
+    * Get the list of all SOPInstanceUID in the active directory.
+    */
   private def getSopList: Seq[String] = ClientUtil.listFiles(activeDir).flatMap(f => fileToSopInstanceUID(f))
 
   /**
-   * Attempt to get an entire series with one DICOM C-MOVE.
-   *
-   * This should always work, but it seems that the Varian VMSDBD daemon sometimes only
-   * sends a partial list of files.
-   */
+    * Attempt to get an entire series with one DICOM C-MOVE.
+    *
+    * This should always work, but it seems that the Varian VMSDBD daemon sometimes only
+    * sends a partial list of files.
+    */
   private def getEntireSeries(SeriesInstanceUID: String): Seq[String] = {
     val specification = new AttributeList
 
@@ -151,9 +153,9 @@ object DicomMove extends Logging {
   }
 
   /**
-   * Perform a C-FIND multiple times and require the maximum slice count multiple times before it
-   * is considered credible.
-   */
+    * Perform a C-FIND multiple times and require the maximum slice count multiple times before it
+    * is considered credible.
+    */
   @tailrec
   private def getCredibleSliceList(SeriesInstanceUID: String, history: Seq[Seq[String]] = Seq()): Seq[String] = {
     // At least this many C-FINDS must return the same result before we believe it.
@@ -161,7 +163,6 @@ object DicomMove extends Logging {
 
     // Wait this many ms between C-FINDs to avoid overloading the server
     val cFindWaitInterval_ms = 250
-
 
     // the maximum number of slices found with multiple attempts
     val maxSliceCount = if (history.isEmpty) 0 else history.maxBy(h => h.size).size
@@ -180,73 +181,76 @@ object DicomMove extends Logging {
   }
 
   /**
-   * Get all files for the given series.  On failure return None and log an error message.
-   *
-   * @param SeriesInstanceUID : Get this series
-   * @param description       : Text used to log descriptive messages
-   */
-  def get(SeriesInstanceUID: String, description: String): Option[Series] = activeDirName.synchronized({
-    clearActiveDir()
+    * Get all files for the given series.  On failure return None and log an error message.
+    *
+    * @param SeriesInstanceUID : Get this series
+    * @param description       : Text used to log descriptive messages
+    */
+  def get(SeriesInstanceUID: String, description: String): Option[Series] =
+    activeDirName.synchronized({
+      clearActiveDir()
 
-    // Get the SOP UID list via C-FIND.
-    val sopCFindList = getCredibleSliceList(SeriesInstanceUID)
+      // Get the SOP UID list via C-FIND.
+      val sopCFindList = getCredibleSliceList(SeriesInstanceUID)
 
-    /**
-     * Log a message, marked the series as failed (so it will not be tried in the future), and return None.  Note
-     * that the list of series marked as failed will be reset when the server restarts, at which point they will
-     * be tried again.
-     */
-    def failed(msg: String) = {
-      logger.warn(description + " " + msg)
-      FailedSeries.put(SeriesInstanceUID)
-      None
-    }
-
-    @tailrec
-    def getAll(retry: Int): Option[Series] = {
-
-      logger.warn("trying series " + description + "    retry count " + retry)
-
-      if (ClientConfig.DICOMRetryCount >= retry) {
-        val sopCMoveList = getEntireSeries(SeriesInstanceUID)
-
-        logger.info("Number of slices received for " + description + " " + SeriesInstanceUID + " : " + sopCMoveList.size)
-        val diff = sopCFindList.size - sopCMoveList.size
-        if (diff == 0) {
-          logger.info("Successfully got " + sopCMoveList.size + " slices on try " + retry)
-          moveActiveDirToSeriesDir
-        } else {
-          if (diff < 0) {
-            logger.warn(description + "C-FIND returned " + sopCFindList.size + " results but C-MOVE returned more: " + sopCMoveList.size + ".  This should never happen.  Retrying C-MOVE.")
-          } else {
-            logger.warn(description + " C-MOVE returned only " + sopCMoveList.size + " files when C-FIND found " + sopCFindList.size)
-
-            logger.info(description + " DicomMove.get Retry " + (1 + ClientConfig.DICOMRetryCount - retry) + " of C-MOVE for series " + SeriesInstanceUID)
-            Thread.sleep((ClientConfig.DICOMRetryWait_sec * 1000).toLong)
-          }
-          getAll(retry + 1)
-        }
-      } else {
-        failed(" Giving up on getting series " + SeriesInstanceUID + " via C-MOVE after retrying " + ClientConfig.DICOMRetryCount +
-          " times.    It will be ignored until this service restarts.")
+      /**
+        * Log a message, marked the series as failed (so it will not be tried in the future), and return None.  Note
+        * that the list of series marked as failed will be reset when the server restarts, at which point they will
+        * be tried again.
+        */
+      def failed(msg: String) = {
+        logger.warn(description + " " + msg)
+        FailedSeries.put(SeriesInstanceUID)
+        None
       }
-    }
 
-    if (sopCFindList.isEmpty) { // if no slices, then never bother again (until next server restart)
-      failed("C-FIND could not find any slices for this series: " + SeriesInstanceUID)
-    } else {
-      val result = getAll(1)
+      @tailrec
+      def getAll(retry: Int): Option[Series] = {
 
-      // the active directory should be deleted if it still exists
-      if (activeDir.exists) Utility.deleteFileTree(activeDir)
+        logger.warn("trying series " + description + "    retry count " + retry)
 
-      result
-    }
-  })
+        if (ClientConfig.DICOMRetryCount >= retry) {
+          val sopCMoveList = getEntireSeries(SeriesInstanceUID)
+
+          logger.info("Number of slices received for " + description + " " + SeriesInstanceUID + " : " + sopCMoveList.size)
+          val diff = sopCFindList.size - sopCMoveList.size
+          if (diff == 0) {
+            logger.info("Successfully got " + sopCMoveList.size + " slices on try " + retry)
+            moveActiveDirToSeriesDir
+          } else {
+            if (diff < 0) {
+              logger.warn(description + "C-FIND returned " + sopCFindList.size + " results but C-MOVE returned more: " + sopCMoveList.size + ".  This should never happen.  Retrying C-MOVE.")
+            } else {
+              logger.warn(description + " C-MOVE returned only " + sopCMoveList.size + " files when C-FIND found " + sopCFindList.size)
+
+              logger.info(description + " DicomMove.get Retry " + (1 + ClientConfig.DICOMRetryCount - retry) + " of C-MOVE for series " + SeriesInstanceUID)
+              Thread.sleep((ClientConfig.DICOMRetryWait_sec * 1000).toLong)
+            }
+            getAll(retry + 1)
+          }
+        } else {
+          failed(
+            " Giving up on getting series " + SeriesInstanceUID + " via C-MOVE after retrying " + ClientConfig.DICOMRetryCount +
+              " times.    It will be ignored until this service restarts."
+          )
+        }
+      }
+
+      if (sopCFindList.isEmpty) { // if no slices, then never bother again (until next server restart)
+        failed("C-FIND could not find any slices for this series: " + SeriesInstanceUID)
+      } else {
+        val result = getAll(1)
+
+        // the active directory should be deleted if it still exists
+        if (activeDir.exists) Utility.deleteFileTree(activeDir)
+
+        result
+      }
+    })
 
   /**
-   * Initialize by starting the DICOM receiver, but do not fetch any data.
-   */
+    * Initialize by starting the DICOM receiver, but do not fetch any data.
+    */
   def init: String = {
     logger.info("initializing DicomMove")
     dicomReceiver.mainDirName
